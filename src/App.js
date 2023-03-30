a928c0e072906a198674fa07a5d304f55394f87a
Mohammad Ashif <mohammadashif510@gmail.com>
2023-03-30 13:06:45 +0600

    add multiple item problem solved

--- src/App.js
+++ src/App.js
@@ -5,31 +5,48 @@ import Fruit from "./Fruit/Fruit";
 import Cart from "./Cart/Cart";
 
 function App() {
-  const [fruits, setFruits] = useState([]);
-  const [cart, setCart] = useState([]);
-  useEffect(() => {
-    fetch("fake.json")
-      .then((res) => res.json())
-      .then((data) => setFruits(data));
-  }, []);
+    const [fruits, setFruits] = useState([]);
+    const [cart, setCart] = useState([]);
 
-  const addToCart = (fruit) => {
-    const newCart = [...cart, fruit];
-    setCart(newCart);
-  }
-  return (
-    <div className="fruits-shop">
-      <h1><span className="name-color">Anik's</span> Juice Shop</h1>
-      <div className="fruits-container">
-        {fruits.map((fruit) => (
-          <Fruit fruit={fruit} key={fruit.id} addToCart = {addToCart}></Fruit>
-        ))}
-      </div>
-      <div>
-        <Cart cart={cart}></Cart>
-      </div>
-    </div>
-  );
+    useEffect(() => {
+        fetch("fake.json")
+            .then((res) => res.json())
+            .then((data) => setFruits(data));
+    }, []);
+
+    const addToCart = (fruit) => {
+        let isPresent = false;
+        cart.forEach(item => {
+          if(item.id === fruit.id){
+            isPresent = true
+          }
+        })
+        if(isPresent){
+          return;
+        }
+        else {
+          const newCart = [...cart, fruit];
+          setCart(newCart);
+        }
+    };
+
+    return (
+        <div className="fruits-shop">
+            <h1>All Items</h1>
+            <div className="fruits-container">
+                {fruits.map((fruit) => (
+                    <Fruit
+                        fruit={fruit}
+                        key={fruit.id}
+                        addToCart={addToCart}
+                    ></Fruit>
+                ))}
+            </div>
+            <div>
+                <Cart cart={cart} setCart={setCart}></Cart>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./Cart.css";
 
-const Cart = ({cart}) => {
+const Cart = ({ cart, setCart }) => {
+    // console.log(cart);
+
+    const handleRemove = (item) => {
+        console.log(item);
+    }
+
     return (
         <div>
-            <h1>Cart item : {cart.length}</h1>
+            {cart.map((item) => (
+                <div key={item.id} className="cart-item">
+                    <div className="img-name">
+                        <img src={item.picture} alt="" />
+                        <h4>{item.name}</h4>
+                    </div>
+                    <div>
+                        <h4>{item.price}</h4>
+                    </div>
+                    <button onClick={() => handleRemove(item)}>Remove</button>
+                </div>
+            ))}
         </div>
     );
 };
